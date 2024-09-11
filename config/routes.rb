5732298f9985d96bba09848d915018f8e659b100
Hieu Tran <trandanghieu2432002@gmail.com>
2024-09-11 11:13:39 +0700

    feature/static-pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
-  root 'application#hello'
+ 
+  root 'static_pages#home'
+
+  get 'static_pages/home'
+  get 'static_pages/help'
+  get 'static_pages/about'
+  get 'static_pages/contact'
+
+
+  get 'foo/bar'
+  get 'foo/baz'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
