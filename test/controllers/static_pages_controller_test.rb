dbda3bc864895bc1f6c17d3ca052651a883d44c4
Hieu Tran <trandanghieu2432002@gmail.com>
2024-09-13 23:34:07 +0700

    Chapter5- Fix Conflict

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -5,44 +5,27 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     @base_title = 'Ruby on Rails Tutorial Sample App'
   end
 
-<<<<<<< chapter5-filling-in-layout
   test 'should get home' do
-      get root_path
-      assert_response :success
-      assert_select 'title', @base_title
-    end
-=======
-  test 'should get root' do
-    get root_url
+    get root_path
     assert_response :success
-    assert_select 'title', "#{@base_title}"
+    assert_select 'title', @base_title
   end
-
-  test 'should get home' do
-    get static_pages_home_url
-    assert_response :success
-    assert_select 'title', "#{@base_title}"
-  end
->>>>>>> master
 
   test 'should get help' do
-    base_title = 'Ruby on Rails Tutorial Sample App'
     get help_path
     assert_response :success
-    assert_select 'title', "Help | #{base_title}"
+    assert_select 'title', "Help | #{@base_title}"
   end
 
   test 'should get about' do
-    base_title = 'Ruby on Rails Tutorial Sample App'
     get about_path
     assert_response :success
-    assert_select 'title', "About | #{base_title}"
+    assert_select 'title', "About | #{@base_title}"
   end
 
   test 'should get contact' do
-    base_title = 'Ruby on Rails Tutorial Sample App'
     get contact_path
     assert_response :success
-    assert_select 'title', "Contact | #{base_title}"
+    assert_select 'title', "Contact | #{@base_title}"
   end
 end
