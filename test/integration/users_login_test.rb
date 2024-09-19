75604d8fbcd65a4ab66c55044fbcb0ae9c7deb80
Hieu Tran <trandanghieu2432002@gmail.com>
2024-09-19 20:58:12 +0700

    Chapter 8 - Basic Login - Final

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -6,15 +6,15 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   end
 
   test 'login with valid email/invalid password' do
-      get login_path
-      assert_template 'sessions/new'
-      post login_path, params: {session: {email: @user.email, password: 'invalid'}}
-      assert_not is_logged_in?
-      assert_template 'sessions/new'
-      assert_not flash.empty?
-      get root_path
-      assert_predicate flash, :empty?
-    end
+    get login_path
+    assert_template 'sessions/new'
+    post login_path, params: {session: {email: @user.email, password: 'invalid'}}
+    assert_not is_logged_in?
+    assert_template 'sessions/new'
+    assert_not flash.empty?
+    get root_path
+    assert_predicate flash, :empty?
+  end
 
   test 'login with valid information followed by logout' do
     get login_path
