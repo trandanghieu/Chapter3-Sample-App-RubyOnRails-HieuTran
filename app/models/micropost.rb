0c91498252b952868bcdcc3fc7e1749f48f400f5
Hieu Tran <trandanghieu2432002@gmail.com>
2024-09-25 23:36:50 +0700

    Chapter 13: User Micropost - Done

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -3,7 +3,8 @@ class Micropost < ApplicationRecord
 
   has_one_attached :image
 
-  default_scope -> { order(created_at: :desc) }
+  scope :recent, -> { order(created_at: :desc) }
+  scope :by_user, ->(user_id) { where(user_id: user_id) }
 
   validates :user_id, presence: true
   validates :content, presence: true, length: {maximum: 140}
--- app/models/user.rb
+++ app/models/user.rb
@@ -65,7 +65,7 @@ class User < ApplicationRecord
   end
 
   def feed
-    Micropost.where('user_id = ?', id)
+    Micropost.by_user(id).recent
   end
 
   private
